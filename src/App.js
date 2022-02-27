b7c6aec59b485250fd2174e100c1689f33a3bf21
suhaas <suhaas.teja@gmail.com>
2022-02-27 19:20:47 +0530

    commiting files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";
+import { data } from "./slotsData";
 
 function App() {
+  const [toggleSidenav, setToggleSidenav] = useState(true);
+  const [availableSlots, setAvailableSlots] = useState([]);
+
+  const handleAvailableSlots = (arr) => {
+    let a = [];
+
+    for (let i = arr[0].hour; i <= arr[1].hour; i++) {
+      a.push(`${i - 1} PM - ${i} PM`);
+    }
+    a.shift();
+    setAvailableSlots(a);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header>
+        <button
+          id="sidenavToggleBtn"
+          onClick={() => setToggleSidenav(!toggleSidenav)}
         >
-          Learn React
-        </a>
+          {toggleSidenav ? (
+            <AiOutlineMenu size={26} />
+          ) : (
+            <AiOutlineArrowLeft size={26} />
+          )}
+        </button>
+        <h1>
+          Mentor<span style={{ color: "crimson" }}>Plus</span>
+        </h1>
       </header>
+
+      <main>
+        <div className={`sidenav-container ${toggleSidenav ? "hidden" : null}`}>
+          <section className="sidenav-content">
+            <article>
+              <button>Home</button>
+            </article>
+            <article>
+              <button>Profile</button>
+            </article>
+          </section>
+        </div>
+        <div
+          className={`demo-container ${toggleSidenav ? null : "demo-hidden"}`}
+        >
+          <h2>Book Demo Session Slot</h2>
+          <section className="slots-date-container">
+            <h4>Select Date</h4>
+            <article className="dateBtns">
+              {data.map((slot) => {
+                return (
+                  <button
+                    id="dateBtn"
+                    key={slot.date}
+                    onClick={() => handleAvailableSlots(slot.available)}
+                  >
+                    {new Date(slot.date).toDateString()}
+                  </button>
+                );
+              })}
+            </article>
+          </section>
+
+          <section className="slots-container">
+            <h4>Select Slot</h4>
+            <article>
+              {availableSlots.map((slot, index) => {
+                return (
+                  <button id="slotBtn" key={index}>
+                    {slot}
+                  </button>
+                );
+              })}
+            </article>
+          </section>
+
+          <button id="paymentBtn">Proceed to pay</button>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/slotsData.js
@@ -0,0 +1,54 @@
+export const data = [
+  {
+    date: "2022-03-22T00:00:00.000Z",
+    available: [
+      {
+        hour: 6,
+        min: 30,
+      },
+      {
+        hour: 7,
+        min: 0,
+      },
+    ],
+  },
+  {
+    date: "2022-03-23T00:00:00.000Z",
+    available: [
+      {
+        hour: 5,
+        min: 0,
+      },
+      {
+        hour: 7,
+        min: 0,
+      },
+    ],
+  },
+  {
+    date: "2022-03-24T00:00:00.000Z",
+    available: [
+      {
+        hour: 5,
+        min: 0,
+      },
+      {
+        hour: 7,
+        min: 0,
+      },
+    ],
+  },
+  {
+    date: "2022-03-25T00:00:00.000Z",
+    available: [
+      {
+        hour: 2,
+        min: 0,
+      },
+      {
+        hour: 4,
+        min: 0,
+      },
+    ],
+  },
+];
